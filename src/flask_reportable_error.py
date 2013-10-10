6aa853aa752850f3d001281c6028913e934c1eb2
Cacilhας, La Batalema <batalema@cacilhas.info>
2013-10-09 23:24:56 -0300

    first commit - reportable error

---
+++ src/flask_reportable_error.py
@@ -0,0 +1,69 @@
+# coding: UTF-8
+# @copyright ©2013, Rodrigo Cacilhας <batalema@cacilhas.info>
+
+from functools import wraps
+import logging
+try:
+    from sqlalchemy.exc import DontWrapMixin
+except ImportError:
+    DontWrapMixin = object
+
+
+__all__ = ['init', 'ReportableErrorMixin', 'reportable']
+
+
+def init(app):
+    ReportableErrorMixin.app = app
+
+    @app.errorhandler(ReportableErrorMixin)
+    def reportable_error_handler(exc):
+        config = app.config.get('REPORTABLE_ERROR', {})
+        loglevel = config.get('LOGLEVEL', logging.DEBUG)
+        app.logger.log(loglevel, '(%s) %s', type(exc).__name__, exc)
+        return exc.report(), exc.status_code, {}
+
+
+class ReportableErrorMixin(Exception, DontWrapMixin):
+
+    app = None
+    _status_code = None
+
+    def report(self):
+        return unicode(self)
+
+    @property
+    def status_code(self):
+        if self._status_code is None:
+            config = self.app.config.get('REPORTABLE_ERROR', {})
+            return config.get('DEFAULT_STATUS_CODE', 500)
+        else:
+            return self._status_code
+
+    @status_code.setter
+    def status_code(self, value):
+        self._status_code = value
+
+
+def single_argument_memoize(f):
+    memo = {}
+
+    @wraps(f)
+    def wrapper(arg):
+        resp = memo.get(arg)
+        if resp is None:
+            resp = memo[arg] = f(arg)
+        return resp
+
+    return wrapper
+
+
+@single_argument_memoize
+def reportable(exception):
+    if issubclass(exception, ReportableErrorMixin):
+        return exception
+
+    return type(
+        'Reportable{0.__name__}'.format(exception),
+        (exception, ReportableErrorMixin),
+        {},
+    )
---
+++ src/test.py
@@ -0,0 +1,118 @@
+#!/usr/bin/env python
+# coding: UTF-8
+# @copyright ©2013, Rodrigo Cacilhας <batalema@cacilhas.info>
+
+import logging
+from unittest import main, TestCase
+import flask_reportable_error
+
+
+class TestInit(TestCase):
+
+    class Application(object):
+
+        def __init__(self):
+            self.handlers = {}
+            self.config = {}
+            self.logged = []
+            self.logger = type('Logger', (object, ), {
+                'log': lambda logger, *args: self.logged.append(args),
+            })()
+
+        def errorhandler(self, exc):
+            def register(callback):
+                self.handlers[exc] = callback
+            return register
+
+    def setUp(self):
+        cls = type(self)
+        app = self.app = cls.Application()
+        flask_reportable_error.init(app)
+        self.handler = app.handlers[
+            flask_reportable_error.ReportableErrorMixin
+        ]
+
+    def test_register_application(self):
+        self.assertEqual(self.handler.__name__,
+                         'reportable_error_handler')
+        self.assertEqual(flask_reportable_error.ReportableErrorMixin.app,
+                         self.app)
+
+    def test_handle_error_500(self):
+        s = 'test reportable error'
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        report, status_code, headers = self.handler(exc)
+        self.assertEqual(report, s)
+        self.assertEqual(status_code, 500)
+        self.assertEqual(headers, {})
+
+    def test_log_error(self):
+        s = 'test reportable error'
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        self.handler(exc)
+        self.assertEqual(self.app.logged, [
+            (logging.DEBUG, '(%s) %s', 'ReportableErrorMixin', exc),
+        ])
+
+    def test_log_deeper(self):
+        s = 'test reportable error'
+        app = self.app
+        app.config['REPORTABLE_ERROR'] = {
+            'LOGLEVEL': logging.ERROR,
+        }
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        self.handler(exc)
+        self.assertEqual(app.logged, [
+            (logging.ERROR, '(%s) %s', 'ReportableErrorMixin', exc),
+        ])
+
+    def test_handle_error_400(self):
+        s = 'test reportable error'
+        self.app.config['REPORTABLE_ERROR'] = {
+            'DEFAULT_STATUS_CODE': 400,
+        }
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        report, status_code, headers = self.handler(exc)
+        self.assertEqual(report, s)
+        self.assertEqual(status_code, 400)
+        self.assertEqual(headers, {})
+
+    def test_handle_own_status(self):
+        s = 'test reportable error'
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        exc.status_code = 404
+        report, status_code, headers = self.handler(exc)
+        self.assertEqual(report, s)
+        self.assertEqual(status_code, 404)
+        self.assertEqual(headers, {})
+
+
+
+class TestReportableErrorMixin(TestCase):
+
+    def test_reportable_error_report(self):
+        s = 'test reportable error'
+        exc = flask_reportable_error.ReportableErrorMixin(s)
+        self.assertEqual(exc.report(), s)
+
+    def test_reportable_factory_return_reportable_error(self):
+        exc_class = flask_reportable_error.reportable(ValueError)
+        self.assertTrue(issubclass(exc_class,
+                                   flask_reportable_error.ReportableErrorMixin))
+        self.assertTrue(issubclass(exc_class, ValueError))
+
+    def test_reportable_factory_response_report_error(self):
+        s = 'test reportable error'
+        exc = flask_reportable_error.reportable(ValueError)(s)
+        self.assertEqual(exc.report(), s)
+
+    def test_reportable_factory_response_be_memoized(self):
+        exc1 = flask_reportable_error.reportable(ValueError)
+        exc2 = flask_reportable_error.reportable(ValueError)
+        exc3 = flask_reportable_error.reportable(AttributeError)
+        self.assertIs(exc1, exc2)
+        self.assertNotEqual(exc1, exc3)
+
+
+if __name__ == '__main__':
+    main()
