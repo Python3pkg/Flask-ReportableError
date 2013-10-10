ed3b2b1b354b65922946c21e9a425cdc29cd1c81
Rodrigo Cacilhas <rodrigo.cacilhas@myfreecomm.com.br>
2013-10-10 18:06:35 -0300

    long_description exists if readme exists

--- setup.py
+++ setup.py
@@ -2,66 +2,13 @@
 from distutils.core import setup
 from os import path
 
-"""
-=====================
-Flask-ReportableError
-=====================
+readme = path.join(path.dirname(__file__), 'README.rst')
+if path.exists(readme):
+    with open(readme) as fd:
+        long_description = fd.read()
 
-Introduction
-------------
-
-Flask-ReportableError is an Flask extension for handling errors that can
-be reported to the web client.
-
-
-Documentation
--------------
-
-In order to use Flask-ReportableError, you must include the following
-statement in you application start script::
-
-    from flask import Flask
-    import flask_reportable_error
-
-    app = Flask(__name__)
-    app.config.from_envvar('FLASK_SETTINGS', silent=True)
-    flask_reportable_error.init(app)
-
-
-API
----
-
-- ``flask_reportable_error.reportable``:
-  factory to create reportable exception classes. For example::
-
-    raise reportable(ValueError)('invalid data received')
-
-
-- ``flask_reportable_error.ReportableErrorMixin``:
-  mixin for reportable exception classes.
-
-  - ``report()``:
-    method that returns the reportable string – can be overridden.
-
-  - ``status_code``:
-    property representing the numeric status code – can be set at
-    instance level.
-
-
-Settings
---------
-
-The Flask settings may contain the key ``REPORTABLE_ERROR``, that’s a
-dictionary with the following keys:
-
-- ``LOGLEVEL``:
-  the logging level. If not supplied, Flask-ReportableError uses
-  ``logging.ERROR``.
-
-- ``DEFAULT_STATUS_CODE``:
-  the default numeric status code for reportable exception classes. By
-  default is 500.
-"""
+else:
+    long_description = None
 
 
 setup(
@@ -75,7 +22,7 @@ setup(
     author           = 'Rodrigo Cacilhας',
     author_email     = 'batalema@cacilhas.info',
     description      = 'handle errors that can be reported to the web client',
-    long_description = __doc__,
+    long_description = long_description,
     install_requires = ['Flask>=0.10.1'],
     classifiers      = [
         'Environment :: Web Environment',
