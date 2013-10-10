99800f475244f88620028d4f6e69739c5699525c
Rodrigo Cacilhas <rodrigo.cacilhas@myfreecomm.com.br>
2013-10-10 16:38:53 -0300

    fix programming language setup.py specs

--- setup.py
+++ setup.py
@@ -24,8 +24,9 @@ setup(
         'License :: OSI Approved :: BSD License',
         'Operating System :: OS Independent',
         'Programming Language :: Python',
+        'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: PyPy',
+        'Programming Language :: Python :: Implementation :: PyPy',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
